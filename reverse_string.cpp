3f6e066ea4ec7a028721b958dfcead73b5fbbd7b
SecretDiscorder <139457966+SecretDiscorder@users.noreply.github.com>
2023-10-18 13:06:26 +0700

    Update reverse_string.cpp

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string inputString;
+
+    cout << "Masukkan sebuah string: ";
+    getline(cin, inputString);
+
+    // Mencetak string terbalik
+    cout << "String terbalik: ";
+    for (int i = inputString.length() - 1; i >= 0; i--) {
+        cout << inputString[i];
+    }
+    cout << endl;
+
+    return 0;
+}
 
